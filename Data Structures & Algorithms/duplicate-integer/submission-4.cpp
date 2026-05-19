2b0de687fd26f5ed6926d4cdc263d2098dc0cbd1
SubhashMurmu <146713584+SubhashMurmu@users.noreply.github.com>
2026-05-19 07:17:15 +0530

    Bulk sync: 14 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_map<int,bool> hash ;
+        for(int i = 0 ; i<nums.size() ; i++ ){
+            if(hash[nums[i]] == true){
+                return true ;
+            }
+            else{
+                hash[nums[i]] = true ;
+            }
+        }
+        return false ;
+    }
+};
